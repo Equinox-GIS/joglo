304176391a203f2e59e574981f0a9ffd45d9cca4
Fahmii19 <fhmialfi79@gmail.com>
2023-08-08 17:55:27 +0700

    memnampilkan popup dari card secara dinamis

---
+++ main.js
@@ -0,0 +1,210 @@
+// function toggle
+function toggleDropdown(id) {
+  const menuButtons = document.querySelectorAll("[id^='menu-button-']");
+  const dropdownMenus = document.querySelectorAll("[id^='dropdown-menu-']");
+  const selectedMenuButton = document.getElementById(`menu-button-${id}`);
+  const selectedDropdownMenu = document.getElementById(`dropdown-menu-${id}`);
+  const isExpanded =
+    selectedMenuButton.getAttribute("aria-expanded") === "true";
+
+  // Toggle the current dropdown menu
+  selectedMenuButton.setAttribute("aria-expanded", !isExpanded);
+  selectedDropdownMenu.classList.toggle("hidden", isExpanded);
+
+  // Rotate arrow
+  const arrow = selectedMenuButton.querySelector("svg");
+  arrow.style.transform = isExpanded ? "rotate(0deg)" : "rotate(180deg)";
+
+  // Close all other dropdown menus
+  menuButtons.forEach((button, index) => {
+    if (button.id !== selectedMenuButton.id) {
+      button.setAttribute("aria-expanded", false);
+      dropdownMenus[index].classList.add("hidden");
+
+      // Rotate other arrows back
+      const otherArrow = button.querySelector("svg");
+      otherArrow.style.transform = "rotate(0deg)";
+    }
+  });
+}
+
+// ---------------------------------------------------------------------------------------------
+
+// Inisialisasi peta Mapbox
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWVudGhvZWxzciIsImEiOiJja3M0MDZiMHMwZW83MnVwaDZ6Z2NhY2JxIn0.vQFxEZsM7Vvr-PX3FMOGiQ";
+
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+  zoom: 14.5,
+  center: [106.8295257, -6.210588],
+  preserveDrawingBuffer: true,
+});
+
+// Fungsi untuk menambahkan titik
+const IzinAjib = () => {
+  fetch(
+    `http://localhost:8000/geojson_izin_galian/galian_utilitas_ajib.geojson`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data); // Mencetak data ke konsol
+      map.addSource("layer-ajib-galian", {
+        type: "geojson",
+        data: data,
+      });
+
+      // Menambahkan lapisan setelah sumber berhasil ditambahkan
+      map.addLayer({
+        id: "layer-ajib-galian",
+        type: "circle",
+        source: "layer-ajib-galian",
+        paint: {
+          "circle-color": "#ef1e28",
+          "circle-stroke-color": "#ef1e28",
+          "circle-stroke-width": 1,
+          "circle-radius": 4,
+          "circle-opacity": 0.8,
+        },
+        layout: {
+          visibility: "visible",
+        },
+      });
+    })
+    .catch((error) => {
+      console.error("Terjadi kesalahan:", error); // Menangani potensi kesalahan
+    });
+
+  // Defining the popup
+  const popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false,
+  });
+
+  // Pop up
+  map.on("mouseenter", "layer-ajib-galian", (e) => {
+    map.getCanvas().style.cursor = "pointer";
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const data = e.features[0].properties; // Extract properties from the event object
+    const content = `
+  <div class="p-0 w-full">
+    <div class="">
+      <h6 class="mt-0 text-sm leading-4 mb-2">${data["kegiatan"]}</h6>
+      <div class="border-b mb-2"></div>
+      <div class="leading-5">
+        <div class="grid grid-cols-2">
+          <div><span>NO SK</span></div>
+          <div><span class="block truncate"> ${data["nama_obj"]}</span></div>
+        </div>
+        <div class="grid grid-cols-2">
+          <div><span>Izin</span></div>
+          <div><span class="block truncate"> ${data["nama_petugas"]}</span></div>
+        </div>
+        <div class="grid grid-cols-2">
+          <div><span>Badan Usaha</span></div>
+          <div><span class="block truncate"> ${data["pemilik"]}</span></div>
+        </div>
+        <div class="grid grid-cols-2">
+          <div><span>Jenis Usaha</span></div>
+          <div><span class="block truncate"> ${data["statusproyek"]}</span></div>
+        </div>
+        <div class="grid grid-cols-2">
+          <div><span>Pelaksana</span></div>
+          <div><span class="block truncate"> ${data["komentar"]}</span></div>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+
+    popup.setLngLat(coordinates).setHTML(content).addTo(map);
+
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
+  });
+
+  // map.on("mouseleave", "layer-ajib-galian", () => {
+  //   map.getCanvas().style.cursor = "";
+  //   popup.remove();
+  // });
+};
+
+// Panggil fungsi setelah peta selesai dimuat
+map.on("load", IzinAjib);
+
+// ---------------------------------------------------------------------------------------------
+
+let currentPopup; // Menyimpan referensi ke popup saat ini
+
+function getDataCard(element) {
+  // Tutup popup saat ini jika ada
+  if (currentPopup) {
+    currentPopup.remove();
+  }
+
+  const activePanduan = parseInt(element.getAttribute("data-card-map"));
+
+  // Mendefinisikan popup
+  const popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false,
+  });
+
+  // Loop melalui sumber data geojson untuk mencari OBJECTID yang cocok
+  map.getSource("layer-ajib-galian")._data.features.forEach((feature) => {
+    if (feature.properties.OBJECTID === activePanduan) {
+      const coordinates = feature.geometry.coordinates.slice();
+      const data = feature.properties;
+      const content = `
+      <div class="p-0 w-full">
+        <div class="">
+          <h6 class="mt-0 text-sm leading-4 mb-2">${data["OBJECTID"]}</h6>
+          <h6 class="mt-0 text-sm leading-4 mb-2">${data["kegiatan"]}</h6>
+          <div class="border-b mb-2"></div>
+          <div class="leading-5">
+            <div class="grid grid-cols-2">
+              <div><span>NO SK</span></div>
+              <div><span class="block truncate"> ${data["nama_obj"]}</span></div>
+            </div>
+            <div class="grid grid-cols-2">
+              <div><span>Izin</span></div>
+              <div><span class="block truncate"> ${data["nama_petugas"]}</span></div>
+            </div>
+            <div class="grid grid-cols-2">
+              <div><span>Badan Usaha</span></div>
+              <div><span class="block truncate"> ${data["pemilik"]}</span></div>
+            </div>
+            <div class="grid grid-cols-2">
+              <div><span>Jenis Usaha</span></div>
+              <div><span class="block truncate"> ${data["statusproyek"]}</span></div>
+            </div>
+            <div class="grid grid-cols-2">
+              <div><span>Pelaksana</span></div>
+              <div><span class="block truncate"> ${data["komentar"]}</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+      popup.setLngLat(coordinates).setHTML(content).addTo(map);
+      currentPopup = popup; // Simpan referensi ke popup saat ini
+
+      while (Math.abs(map.getCenter().lng - coordinates[0]) > 180) {
+        coordinates[0] += map.getCenter().lng > coordinates[0] ? 360 : -360;
+      }
+    }
+  });
+}
+
+function removePopup() {
+  // Tutup popup saat ini jika ada
+  if (currentPopup) {
+    currentPopup.remove();
+    currentPopup = null;
+  }
+}
+
+// ---------------------------------------------------------------------------------------------
