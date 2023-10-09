deefbfea9b278faddd9101fa0b103461cf81c833
Fahmii19 <fhmialfi79@gmail.com>
2023-10-09 17:51:19 +0700

    membuat judul , mengaktifkan kontrol dimap serta menambahakan 1 btn location perbaikan search

--- src/js/main.js
+++ src/js/main.js
@@ -40,7 +40,51 @@ var map = new mapboxgl.Map({
   zoom: 14.5,
   center: [106.8295257, -6.210588],
   preserveDrawingBuffer: true,
+  attributionControl: false,
 });
+
+map.addControl(new mapboxgl.NavigationControl());
+
+var controlGroup = document.querySelector(
+  ".mapboxgl-ctrl-top-right .mapboxgl-ctrl-group"
+);
+
+var compass = controlGroup.querySelector(".mapboxgl-ctrl-compass");
+if (compass) {
+  compass.style.setProperty("display", "none", "important");
+}
+
+// Jika grup kontrol ditemukan
+if (controlGroup) {
+  // Kode untuk tombol kustom Anda di sini
+
+  var myLocationButton = document.createElement("button");
+  myLocationButton.className =
+    "mapboxgl-ctrl-icon custom-control-button my-location-button";
+  var myLocationButton = document.createElement("button");
+  myLocationButton.className = "custom-control-button my-location-button";
+  myLocationButton.setAttribute("type", "button");
+  myLocationButton.setAttribute("aria-label", "My Location");
+  myLocationButton.innerHTML =
+    '<span class="mapboxgl-ctrl-icon" aria-hidden="true" title="My Location"></span>';
+
+  myLocationButton.addEventListener("click", function () {
+    alert("My Location button clicked!");
+  });
+
+  // Dapatkan tombol zoom-in dan zoom-out
+  var zoomInButton = controlGroup.querySelector(".mapboxgl-ctrl-zoom-in");
+  var zoomOutButton = controlGroup.querySelector(".mapboxgl-ctrl-zoom-out");
+
+  // Hapus tombol zoom-in dan zoom-out dari DOM
+  controlGroup.removeChild(zoomInButton);
+  controlGroup.removeChild(zoomOutButton);
+
+  // Menambahkan tombol ke grup kontrol dalam urutan yang diinginkan
+  controlGroup.appendChild(myLocationButton);
+  controlGroup.appendChild(zoomInButton);
+  controlGroup.appendChild(zoomOutButton);
+}
 
 // Fungsi untuk menambahkan titik
 const IzinAjib = () => {
