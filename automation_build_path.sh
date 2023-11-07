cb48ab0202b454f68637965173b011ce181aff29
Asnur Ramdani <asnurramdhani12@gmail.com>
2023-11-07 15:15:05 +0700

    revisi automation code

--- automation_build_path.sh
+++ automation_build_path.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "../joglo_build_dev"
+cd ""
 
 # Get the current date in the format ddmmyyyy
 current_date=$(date +'%d%m%Y')
@@ -8,9 +8,9 @@ folder_name=$current_date
 
 # Check if the folder already exists
 if [ -d "$folder_name" ]; then
-    cp -r "../joglo/* $folder_name"
+    cp -r "./* ../joglo_build_dev/$folder_name"
 else
     # If the folder doesn't exist, create it
     mkdir "$folder_name"
-    cp -r "../joglo/* $folder_name"
+    cp -r "./* ../joglo_build_dev$folder_name"
 fi
