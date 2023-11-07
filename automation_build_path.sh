32979c143578df49bc241e795f98a28f74ae13b5
Asnur Ramdani <asnurramdhani12@gmail.com>
2023-11-07 15:24:39 +0700

    revisi automation code

--- automation_build_path.sh
+++ automation_build_path.sh
@@ -6,9 +6,9 @@ folder_name=$current_date
 
 # Check if the folder already exists
 if [ -d "../joglo_build_dev/$folder_name" ]; then
-    cp -r "./* ../joglo_build_dev/$folder_name"
+    cp -r * ../joglo_build_dev/$folder_name
 else
     # If the folder doesn't exist, create it
     mkdir "../joglo_build_dev/$folder_name"
-    cp -r "./* ../joglo_build_dev/$folder_name"
+    cp -r * ../joglo_build_dev/$folder_name
 fi
