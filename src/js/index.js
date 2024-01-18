f1c33d564f5ea5b5d2d62af95d0fa7e2e6350409
Fahmii19 <fhmialfi79@gmail.com>
2024-01-18 13:56:14 +0700

    push

--- src/js/index.js
+++ src/js/index.js
@@ -877,13 +877,13 @@ function getSliderStoryGaleri() {
     slidesToScroll: 5,
     infinite: false,
     dots: false,
-    prevArrow: `<button type="button" class="slick-prev slick-prev-story" style="margin-left:-47px !important; onclick="event.stopPropagation();">
+    prevArrow: `<button type="button" class="slick-prev slick-prev-story" style="margin-left:-55px !important; onclick="event.stopPropagation();">
     <div>
                   <div class="img-wrapper" style="border: 1.5px solid #afb4b8;">
                     <img style="margin-right: 1px;" class="custom-img-slick" src="${prevArrowImg}" alt="Previous">
                   </div>
                 </button>`,
-    nextArrow: `<button type="button" class="slick-next slick-next-story" style="margin-right:-52px!important; onclick="event.stopPropagation();">
+    nextArrow: `<button type="button" class="slick-next slick-next-story" style="margin-right:-55px!important; onclick="event.stopPropagation();">
                   <div class="img-wrapper" style="border: 1.5px solid #afb4b8;">
                     <img style="margin-left: 1px;" class="custom-img-slick" src="${nextArrowImg}" alt="Next">
                   </div>
