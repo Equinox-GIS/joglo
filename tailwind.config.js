dfc240dece111d95d929c908295e905ae6c77987
Fahmii19 <fhmialfi79@gmail.com>
2024-02-01 16:28:05 +0700

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     },
     extend: {
       fontFamily: {
+        lobster: ['"Lobster"'],
         sf_pro_reguler: ["Font SF-Pro reguler"],
         sf_pro_bold: ["Font SF-Pro bold"],
         sf_pro_medium: ["Font SF-Pro medium"],
