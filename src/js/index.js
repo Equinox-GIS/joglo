d77dc61a615c2eedf60551dddd5d36536ff3892f
Fahmii19 <fhmialfi79@gmail.com>
2024-06-06 16:29:30 +0700

    refactor: Update menu detail buttons styling and layout

--- src/js/index.js
+++ src/js/index.js
@@ -5309,6 +5309,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".menu_pengaturaan_detail_dua div");
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", function () {
+      // Remove active class from all tabs
+      tabs.forEach((t) => t.classList.remove("bg_aktif_detail"));
+      // Add active class to the clicked tab
+      tab.classList.add("bg_aktif_detail");
+    });
+  });
+});
+
 const sliders = document.querySelectorAll(".slider");
 
 function progressScript(sliderEl) {
